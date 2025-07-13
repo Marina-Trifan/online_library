e5cfcff3e2c1bfadf57c75be4fa6c92e29450965
Marina-Trifan <trifan_marina_93@yahoo.com>
2025-07-13 18:55:29 +0300

    Part of authentication process has been created + additional features

--- readira/readira/urls.py
+++ readira/readira/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user_account.views import MainPage, CustomLoginView, RegisterView, logout_view
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("", MainPage.as_view(), name="index"),
 ]
--- readira/user_account/views.py
+++ readira/user_account/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
 
-# Create your views here.
+
+from user_account.forms import SignUpForm
+
+# functional view
+# def main_page(request):
+#     return render(request, template_name="main_page.html", context={"n": range(5)})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Class-Based view (CBV)
+class MainPage(TemplateView):
+    template_name = 'main_page.html'
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    success_url = reverse_lazy('index')
+
+
+    
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
