24409b8eea56d60139a5b3ea0f056821e9675e7a
Marina-Trifan <trifan_marina_93@yahoo.com>
2025-07-20 14:50:15 +0300

    Respective models registered in library/admin.py and user_account/admin.py

--- readira/library/admin.py
+++ readira/library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Author, Genre, BookType, ReadingMaterials, Review, Rating
+
+
 # Register your models here.
+
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookType)
+admin.site.register(ReadingMaterials)
+admin.site.register(Review)
+admin.site.register(Rating)
+
